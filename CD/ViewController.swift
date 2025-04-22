d85802d0ad3b44b809a46b775ebf463df41f77e4
M-Kumail512 <m.kumail@hyernymbiz.com>
2025-04-22 11:59:44 +0500

    Some changes for CI

--- CD/ViewController.swift
+++ CD/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("Practicing CI for the GitHub")
     }
+    
 
 
 }
